ff8d1fe4fdfe046065528ddc36eca53ad7a8e491
fmy <18536815115@163.com>
2019-04-11 08:48:46 +0800

    Submit the activity module for the first time and initialize

---
+++ weekend-activity/src/main/java/xin/stxkfzx/weekend/activity/WeekendActivityApplication.java
@@ -0,0 +1,13 @@
+package xin.stxkfzx.weekend.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekendActivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekendActivityApplication.class, args);
+    }
+
+}
