03f4c8e4896ec2594a014ee9d1b4addd6c648de4
tianzuo <2719969254@qq.com>
2019-04-12 13:51:30 +0800

    user module creation

---
+++ weekend-user/src/test/java/xin/stxkfzx/weekend/user/WeekendUserApplicationTests.java
@@ -0,0 +1,16 @@
+package xin.stxkfzx.weekend.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeekendUserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
