e4546a3bbf079b964503be16bc5c8c91b9469bb7
fmy <18536815115@163.com>
2019-04-18 16:43:14 +0800

    update configuration

---
+++ weekend-activity/src/test/java/xin/stxkfzx/weekend/activity/ActivityBaseTest.java
@@ -0,0 +1,18 @@
+package xin.stxkfzx.weekend.activity;
+
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Rollback
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivityBaseTest {
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+}
---
+++ weekend-activity/src/test/java/xin/stxkfzx/weekend/activity/service/impl/ActivityServiceImplTest.java
@@ -0,0 +1,58 @@
+package xin.stxkfzx.weekend.activity.service.impl;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import xin.stxkfzx.weekend.activity.ActivityBaseTest;
+import xin.stxkfzx.weekend.activity.entity.Activity;
+import xin.stxkfzx.weekend.activity.expand.ActivityExpand;
+import xin.stxkfzx.weekend.activity.service.ActivityService;
+import xin.stxkfzx.weekend.common.exception.CheckException;
+import xin.stxkfzx.weekend.user.entity.User;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author fmy
+ * @date 2019-04-15 11:05
+ */
+public class ActivityServiceImplTest extends ActivityBaseTest {
+    @Autowired
+    private ActivityService service;
+
+    @Test
+    public void createActivity() {
+        Activity activity = new Activity();
+        activity.setUserId(1);
+        activity.setCoordinate("100, 300, 50");
+        activity.setRange((short) 1000);
+        activity.setTitle("创建活动测试标题");
+        activity.setMaxCount(10);
+        activity.setStartTime(new Date());
+        activity.setDescription("创建活动测试内容");
+
+        service.createActivity(activity);
+        assertNotNull(activity.getTbId());
+
+        exception.expect(CheckException.class);
+        service.createActivity(null);
+    }
+
+    @Test
+    public void joinActivity() {
+        User user = new User();
+        user.setTbId(1);
+
+        Activity activity = new Activity();
+        activity.setTbId(123);
+
+        exception.expect(CheckException.class);
+        exception.expectMessage("Invalid id:" + activity.getTbId());
+        ActivityExpand expand = service.joinActivity(user, activity);
+
+        activity.setTbId(1);
+        expand = service.joinActivity(user, activity);
+        assertNotNull(expand.getJoinRecord());
+    }
+}
