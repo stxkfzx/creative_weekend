1063c964996b3d60aa508a25c087c4f7d4435f73
fmy <18536815115@163.com>
2019-04-18 16:49:44 +0800

    add chat module

---
+++ weekend-chat/src/main/java/xin/stxkfzx/weekend/chat/WeekendChatApplication.java
@@ -0,0 +1,13 @@
+package xin.stxkfzx.weekend.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekendChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekendChatApplication.class, args);
+    }
+
+}
