ef6f6dcc79bee0a92989d37d1fd1b5e34316a223
fmy <18536815115@163.com>
2019-04-27 11:21:52 +0800

    first commit by IDEA

---
+++ src/main/java/xin/stxkfzx/weekend/WeekendApplication.java
@@ -0,0 +1,13 @@
+package xin.stxkfzx.weekend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekendApplication.class, args);
+    }
+
+}
