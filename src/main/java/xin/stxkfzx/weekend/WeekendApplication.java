09595a2116c048b518f84aec9379e7418152fbbf
tianzuo <2719969254@qq.com>
2019-04-28 16:01:52 +0800

    Merge multiple modules

--- src/main/java/xin/stxkfzx/weekend/WeekendApplication.java
+++ src/main/java/xin/stxkfzx/weekend/WeekendApplication.java
@@ -3,12 +3,16 @@ package xin.stxkfzx.weekend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-
+/**
+ * 创意周末后台项目入口
+ *
+ * @author CreativeWeekendTeam
+ * @version V1.0
+ * @date 2019/4/13
+ */
 @SpringBootApplication
 @MapperScan("xin.stxkfzx.weekend.mapper")
-@ComponentScan("xin.stxkfzx.weekend")
 public class WeekendApplication {
     public static void main(String[] args) {
         SpringApplication.run(WeekendApplication.class, args);
